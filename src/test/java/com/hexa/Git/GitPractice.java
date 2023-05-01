f05ca00a69a3e2c78c2fad24af15e0e66f67e09a
gokulraj0908 <mailforgokulraj@gmail.com>
2023-05-01 15:25:04 +0530

    added 3 new methods

---
+++ src/test/java/com/hexa/Git/GitPractice.java
@@ -0,0 +1,16 @@
+package com.hexa.Git;
+
+public class GitPractice {
+	public void india() {
+		System.out.println("IND");
+
+	}
+	public void Amerrica() {
+		System.out.println("USA");
+
+	}
+	public void Australia() {
+		System.out.println("AUS");
+
+	}
+}
